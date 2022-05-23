500f80e4679fa93ddb689bbed0aeba73e7bffb3f
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-23 18:41:19 +0600

    features section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,23 @@ export const Home = () => {
       <div className="banner">
         <h2>Fruit is Nature's Candy.</h2>
       </div>
+
+      <div className="key_features">
+        <div className="features">
+          <div className="option">
+            <h3>Delivery</h3>
+            <p>fast delivery</p>
+          </div>
+          <div className="option">
+            <h3>Great choice</h3>
+            <p>choose by professionals</p>
+          </div>
+          <div className="option">
+            <h3>Freash Only</h3>
+            <p>100% Guaranty quality</p>
+          </div>
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
