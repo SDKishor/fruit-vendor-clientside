6629ac92ebed68707033028d113f953f0472f315
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-24 18:46:22 +0600

    fruit detail page design complete

--- src/components/fruitDetails/FruitDetails.jsx
+++ src/components/fruitDetails/FruitDetails.jsx
@@ -20,20 +20,42 @@ export const FruitDetails = () => {
       <div className="fruitContainer">
         <div className="imageContainer">
           <img src={fruit.image} alt="" />
-          <h1 className="fruitName">{fruit.Fruit}</h1>
+          <div className="fruitName">
+            <h1>{fruit.Fruit}</h1>
+            <h3>{fruit.price}</h3>
+          </div>
         </div>
         <div className="contentContainer">
-          <div className="vendorinfo">
-            <div className="vendorImg">
-              <img src={fruit.vendor_image} />
+          <div className="info">
+            <div className="vendorinfo">
+              <div className="vendorImg">
+                <img src={fruit.vendor_image} />
+              </div>
+              <div className="vendordateils">
+                <h2>Vendor: {fruit.vendor}</h2>
+                <h3> {fruit.email}</h3>
+              </div>
             </div>
-            <div className="vendordateils">
-              <h2>Vendor: {fruit.vendor}</h2>
-              <h3> {fruit.email}</h3>
+            <div className="description">
+              <h2>{fruit.description}</h2>
+            </div>
+            <div className="stockInfo">
+              <div className="option">
+                <h3>Quantity</h3>
+                <h4>{fruit.quantity} kg</h4>
+              </div>
+              <div className="option">
+                <h3>Sold</h3>
+                <h4>{fruit.sold}</h4>
+              </div>
             </div>
           </div>
-          <div className="description">
-            <h2>{fruit.description}</h2>
+          <div className="btnContainer">
+            <div className="restockContainer">
+              <input type="number" />
+              <button type="submit">Restock</button>
+            </div>
+            <button className="deliveryBtn">DELIVERED</button>
           </div>
         </div>
       </div>
